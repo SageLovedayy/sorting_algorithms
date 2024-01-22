0f7f32bbe5e04123b7f2a8aaeaeaf1adc6376222
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-01-22 23:01:06 +0100

    Initial commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+* bubble_sort - Implements the bubble sort algorithm for sorting ints
+* in ascending order
+*
+* @array: array to sort
+* @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swapped;
+
+	for (i = 0; i < size - 1; ++i)
+	{
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; ++j)
+		{
+
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				swapped = 1;
+			}
+
+			print_array(array, size);
+		}
+
+		if (swapped == 0)
+		{
+			break;
+		}
+	}
+}
